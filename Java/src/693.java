018069a7144ff4e054f7d431d8252b75ea3a3c81
HHR <i@huhaorui.com>
2021-02-17 14:43:59 +0800

    update

---
+++ Java/src/693.java
@@ -0,0 +1,18 @@
+class Solution_693 {
+    public static void main(String[] args) {
+        System.out.println(new Solution_693().hasAlternatingBits(5));
+    }
+
+    public boolean hasAlternatingBits(int n) {
+        int flag = n % 2;
+        n /= 2;
+        while (n > 0) {
+            if (n % 2 + flag != 1) {
+                return false;
+            }
+            flag = 1 - flag;
+            n /= 2;
+        }
+        return true;
+    }
+}
