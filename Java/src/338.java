6adef14619aa4bb228d78b02a661948fb1cac8ec
HHR <i@huhaorui.com>
2021-02-17 14:17:33 +0800

    update

---
+++ Java/src/338.java
@@ -0,0 +1,14 @@
+class Solution_338 {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        ans[0] = 0;
+        for (int i = 1; i < num; i += 2) {
+            ans[i] = ans[i / 2] + 1;
+            ans[i + 1] = ans[i / 2 + 1];
+        }
+        if (num % 2 == 1) {
+            ans[num] = ans[num / 2] + 1;
+        }
+        return ans;
+    }
+}
