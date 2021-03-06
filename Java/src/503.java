d6fabda1de01c627a2b56bf3d5afc39a6de598a0
HHR <i@huhaorui.com>
2021-03-06 21:22:23 +0800

    add 503.java

---
+++ Java/src/503.java
@@ -0,0 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class Solution_503 {
+    public int[] nextGreaterElements(int[] nums) {
+        int length = nums.length;
+        int[] ans = new int[length];
+        Arrays.fill(ans, -1);
+        Deque<Integer> d = new ArrayDeque<>();
+        for (int i = 0; i < length * 2; i++) {
+            while (!d.isEmpty() && nums[i % length] > nums[d.peekLast()]) {
+                int u = d.pollLast();
+                ans[u] = nums[i % length];
+            }
+            d.addLast(i % length);
+        }
+        return ans;
+    }
+}
