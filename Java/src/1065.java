66543a6d79e9da5363c98f9f36df908fbeb5deb1
HHR <i@huhaorui.com>
2021-03-09 14:13:57 +0800

    update

---
+++ Java/src/1065.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution_1065 {
+    public String removeDuplicates(String S) {
+        if (S.length() == 0) {
+            return "";
+        }
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        stack.push(S.charAt(0));
+        for (int i = 1; i < S.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(S.charAt(i));
+                continue;
+            }
+            if (stack.peek() == S.charAt(i)) {
+                stack.pop();
+            } else {
+                stack.push(S.charAt(i));
+            }
+        }
+        stack.forEach(sb::append);
+        return sb.toString();
+    }
+}
---
+++ Java/src/374.java
@@ -0,0 +1,33 @@
+class GuessGame {
+    int ans = 123;
+
+    int guess(int num) {
+        return Integer.compare(ans, num);
+    }
+}
+
+class Solution_374 extends GuessGame {
+    public static void main(String[] args) {
+        System.out.println(new Solution_374().guessNumber(333));
+    }
+
+    public int guessNumber(int num) {
+        int leftPtr = 1;
+        int rightPtr = num;
+        while (true) {
+            int guess = leftPtr + (rightPtr - leftPtr) / 2;
+            switch (guess(guess)) {
+                case -1: {
+                    rightPtr = guess - 1;
+                    break;
+                }
+                case 0: {
+                    return guess;
+                }
+                case 1: {
+                    leftPtr = guess + 1;
+                }
+            }
+        }
+    }
+}
