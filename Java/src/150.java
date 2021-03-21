2bd0df7a4ea694c04b684b50df49a449feb664da
HHR <i@huhaorui.com>
2021-03-21 19:35:24 +0800

    create 73.java

--- Java/src/150.java
+++ Java/src/150.java
@@ -7,34 +7,30 @@ class Solution_150 {
         System.out.println(new Solution_150().evalRPN(ops));
     }
 
-    boolean isOp(String op) {
-        return op.equals("+") || op.equals("-") || op.equals("/") || op.equals("*");
-    }
-
     public int evalRPN(String[] tokens) {
         Stack<Integer> numbers = new Stack<>();
         Arrays.stream(tokens).forEach(it -> {
-            if (!isOp(it)) {
-                numbers.push(Integer.parseInt(it));
-            } else {
-                switch (it) {
-                    case "+": {
-                        numbers.push(numbers.pop() + numbers.pop());
-                        break;
-                    }
-                    case "-": {
-                        numbers.push(-numbers.pop() + numbers.pop());
-                        break;
-                    }
-                    case "*": {
-                        numbers.push(numbers.pop() * numbers.pop());
-                        break;
-                    }
-                    case "/": {
-                        int num1 = numbers.pop();
-                        int num2 = numbers.pop();
-                        numbers.push(num2 / num1);
-                    }
+            switch (it) {
+                case "+": {
+                    numbers.push(numbers.pop() + numbers.pop());
+                    break;
+                }
+                case "-": {
+                    numbers.push(-numbers.pop() + numbers.pop());
+                    break;
+                }
+                case "*": {
+                    numbers.push(numbers.pop() * numbers.pop());
+                    break;
+                }
+                case "/": {
+                    int num1 = numbers.pop();
+                    int num2 = numbers.pop();
+                    numbers.push(num2 / num1);
+                    break;
+                }
+                default: {
+                    numbers.push(Integer.valueOf(it));
                 }
             }
         });
