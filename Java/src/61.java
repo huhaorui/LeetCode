6e4fb9cee6285636abac3ede82d3e129ec30f85d
HHR <i@huhaorui.com>
2021-03-27 17:47:46 +0800

    create 61.java

--- Java/src/61.java
+++ Java/src/61.java
@@ -8,12 +8,43 @@
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution_61 {
+class Solution {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        head.next = new ListNode(1);
+        System.out.println(new Solution().rotateRight(head, 1));
+    }
+
     public ListNode rotateRight(ListNode head, int k) {
-        ListNode newHead = head;
-        for (int i = 0; i <= k; i++) {
-            newHead = newHead.next;
+        if (head == null) {
+            return null;
         }
+        if (head.next == null) {
+            return head;
+        }
+        int length = 1;
+        ListNode ptr = head;
+        while (ptr.next != null) {
+            length++;
+            ptr = ptr.next;
+        }
+        k %= length;
+        if (k == 0) {
+            return head;
+        }
+        k = length - k;
+        ListNode newHead = head;
+        ListNode prev = new ListNode(-1, newHead);
+        for (int i = 0; i < k; i++) {
+            newHead = newHead.next;
+            prev = prev.next;
+        }
+        ListNode tmp = prev;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        tmp.next = head;
+        prev.next = null;
         return newHead;
     }
 }
