b47afcfa40259aa927e645a7f71f0b37ae2cc309
HHR <i@huhaorui.com>
2021-03-29 19:27:24 +0800

    add 190.java

--- Java/src/leetcode/173.java
+++ Java/src/leetcode/173.java
@@ -26,6 +26,15 @@ class BSTIterator {
         search(root);
     }
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(7);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(9);
+        root.right.right = new TreeNode(20);
+        new BSTIterator(root);
+    }
+
     public void search(TreeNode node) {
         if (node == null) {
             return;
---
+++ Java/src/leetcode/190.java
@@ -0,0 +1,6 @@
+public class Solution_190 {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+}
