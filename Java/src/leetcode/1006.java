c4614e7392a6f6b29f599beb7dde9b5d83b69cf5
胡皓睿 <i@huhaorui.com>
2021-04-01 16:19:15 +0800

    update

---
+++ Java/src/leetcode/1006.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+class Solution_1006 {
+    public int clumsy(int N) {
+        if (N == 1) {
+            return 1;
+        } else if (N == 2) {
+            return 2;
+        } else if (N == 3) {
+            return 6;
+        } else if (N == 4) {
+            return 7;
+        }
+
+        if (N % 4 == 0) {
+            return N + 1;
+        } else if (N % 4 <= 2) {
+            return N + 2;
+        } else {
+            return N - 1;
+        }
+    }
+}
